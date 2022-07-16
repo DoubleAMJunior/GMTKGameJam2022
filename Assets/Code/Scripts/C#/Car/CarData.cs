cd7cb50d3c0e1d057a32c4530e45620113ceb491
CT Arroyo <thekatherynarroyo@gmail.com>
2022-07-17 01:22:18 +0800

    Car Movement Calibration
    torn between car mass = 3 or 1.5

--- Assets/Code/Scripts/C#/Car/CarData.cs
+++ Assets/Code/Scripts/C#/Car/CarData.cs
@@ -1,14 +1,16 @@
+//for bigger tracks or faster movement, start testing from commented values
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu ( fileName = "CarData" , menuName = "Game/Car/CarData",order =0 )]
 public class CarData : ScriptableObject {
-	public float accelerationMax = 10f;
-	public float maxSpeed = 2f;
-	public float accelerationFactor = 5f;
-	public float turnFactor = 3.5f;
-	public float dragValue = 1f;
-	public float dragTime = 1f;
-	public float driftFactor = .95f;
+	public float accelerationMax = 5f; //10f;
+	public float maxSpeed = 10f; //50f;
+	public float accelerationFactor = .01f; //2f;
+	public float turnFactor = 40f;
+	public float dragValue = 100f;
+	public float dragTime = 3f;
+	public float driftFactor = .5f;
 }
