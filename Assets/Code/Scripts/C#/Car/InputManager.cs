d46423d2a1d63dfcb6d17bf449a80ad353a04aab
DoubleAMjunior <aliabbasi1377@gmail.com>
2022-07-16 17:15:01 +0430

    Some Refactor chganges

--- Assets/Code/Scripts/C#/Car/InputManager.cs
+++ Assets/Code/Scripts/C#/Car/InputManager.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour {
-	CarInterface playerMovement;
 
-
+	public UnityEvent<Vector2> directionInput;
 	private void FixedUpdate()
 	{
 		//get x/y input, send as vector to PlayerMovement
 		Vector2 inputVector = Vector2.zero;
 		inputVector.x = Input.GetAxis("Horizontal");
 		inputVector.y = Input.GetAxis("Vertical");
-
-		playerMovement.SetInputVector(inputVector);
+		directionInput?.Invoke(inputVector);
 	}
-
-	//insert collision mechanics
 }
